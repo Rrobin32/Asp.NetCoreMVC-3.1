667b425af93b6cc7ed83772562a32fbd4dc55533
Rakesh <rakesh.k@exaosis.com>
2021-03-03 06:23:59 +0530

    commit middle ware

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -26,14 +26,65 @@ namespace BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+            /* Custome MiddleWare */
+            /* 
+             * We can have "n" number of middleware in a program but the only thing is important is the order of the middleware in which they are used.
+             * "next()" is used to call the next middleware. and the response is in the same manner in whichn the middleware is called.
+             * Asp.Net Core create an HTTP Application Pipeline that processes the request.
+             * The Http Pipleline is congigure in configure method of Startup.cs.
+             * All the request to the application goes through the HTTP pipeline.
+             * An middleware is a piece of code(Component) which is used in http pipeline.
+             * in an application we can use mutliple middleware.
+             * Middleware has access to all the request and response.
+
+             */
+            app.Use(async (Context, next) =>
+                {
+                    await Context.Response.WriteAsync("Hello my first Middleware");
+
+                    await next();
+
+                    await Context.Response.WriteAsync("\nHello my first Middleware after next method is called");
+                });
+
+            app.Use(async (Context, next) =>
+            {
+                await Context.Response.WriteAsync("\nHello my second Middleware");
+
+                await next();
+
+                await Context.Response.WriteAsync("\nHello my second Middleware after next method is called");
+            });
+
+            app.Use(async (Context, next) =>
+                {
+                    await Context.Response.WriteAsync("\nHello my third Middleware");
+
+                    await next();
+                });
+
+            /* To map a url with the particular resource.
+             * Order of middleware is very important if we remove this  "app.UseRouting();" this the program will throw an error.
+             * If we move this middleware to the last then also the program will throw an error.
+             */
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
+                /* mapping a url with the particular resource. */
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("\nHello World!");
                 });
+
+                /* mapping a url with the particular resource. */
+                endpoints.Map("/mvc", async context =>
+                {
+                    await context.Response.WriteAsync("\nWelcome to the ASP.Net Core MVC!");
+                });
+                /* Difference between Map() and MapGet() is MapGet() only handele the get() for the particular Route but Map() will handele all request comming to the 
+                   Particular Route.  
+                 */
             });
         }
     }
